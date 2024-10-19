170bb4e084f5d2568f0802d5751bcdf7caf306ee
Vaibhav-patill <vaibhavpatil00913@gmail.com>
2024-10-19 21:10:36 +0530

    binary operators

---
+++ Bitmanipulation/Binary.java
@@ -0,0 +1,24 @@
+public class Binary {
+    public static void main(String[] args) {
+        System.out.println(5&4);//binary AND operator
+        System.out.println(5|4);//binary OR operator
+        System.out.println(5^4);//binary XOR operator
+        System.out.println(~5);//binary one's  compliment
+    }
+}
+
+// Decimal to binary:Divide the decimal number with 2 to convert into binary
+//Binary to decimal:Multiply each bit  of binary with 2 raised to the power of its position and add all the results
+/*Decimal number in binary from 0 to 10
+0=000
+1=001
+2=010
+3=011
+4=100
+5=101
+6=110
+7=111
+8=1000
+9=1001
+10=1010 
+ */
