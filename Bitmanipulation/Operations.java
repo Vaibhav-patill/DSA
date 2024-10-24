258d82b85d3bd7eccce06a8318c424ecb6074caa
vaibhavpatilll <vaibhav01302@gmail.com>
2024-10-25 00:01:36 +0530

    New file added

--- Bitmanipulation/Operations.java
+++ Bitmanipulation/Operations.java
@@ -1,5 +1,13 @@
 public class Operations {
+    public static int getIthBit(int n,int i){
+        int bitMask=1<<i;
+        if((n & bitMask)==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
     public static void main(String[] args) {
-        
+        getIthBit(10,2);
     }
 }
