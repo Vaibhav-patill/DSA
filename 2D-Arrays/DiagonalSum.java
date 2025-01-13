eebbcc3b7d6e14ebce6b079e4844d4278ae606e0
Vaibhav-patill <vaibhavpatil00913.com>
2025-01-13 21:15:57 +0530

    solved diagonal sum problem

--- 2D-Arrays/DiagonalSum.java
+++ 2D-Arrays/DiagonalSum.java
@@ -2,14 +2,21 @@ public class DiagonalSum {
 
     public static int diagonalsum(int matrix[][]){
         int sum=0;
-        int n=matrix.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(i==j || i+j==n-1){
-                    sum+=matrix[i][j];
-                }else{
-                    continue;
-                }
+        // int n=matrix.length;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(i==j || i+j==n-1){
+        //             sum+=matrix[i][j];
+        //         }else{
+        //             continue;
+        //         }
+        //     }
+        // }
+
+        for(int i=0;i<matrix.length;i++){
+            sum+=matrix[i][i];
+            if(i!=matrix.length -i -1){
+                sum+=matrix[i][matrix.length -i -1];
             }
         }
 
