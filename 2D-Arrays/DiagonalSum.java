40032e2fc847948fe936886936135f632471b4d3
Vaibhav-patill <vaibhavpatil00913@gmail.com>
2025-01-14 09:42:52 +0530

    DSA

--- 2D-Arrays/DiagonalSum.java
+++ 2D-Arrays/DiagonalSum.java
@@ -33,7 +33,7 @@ public class DiagonalSum {
         int matrix[][]={
             {1,2,3},
             {4,5,6},
-            {7,8,9} 
+            {7,8,10} 
         };
 
                 System.err.println(diagonalsum(matrix));
